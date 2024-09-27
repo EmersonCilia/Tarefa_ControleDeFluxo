62bf8d38a62a6f1014df0f521fdd4ec8dda1f575
Emerson <Emersonwpprado@gmail.com>
2024-09-27 15:25:07 -0300

    Tarefa Ebac

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int media1 = 7;
+        int media2 = 8;
+        int media3 = 5;
+        int media4 = 10;
+        double mediaFinal = (double)(media1 + media2 + media3 + media4) / 4;
+
+        System.out.println(mediaFinal);
+    }
+}
