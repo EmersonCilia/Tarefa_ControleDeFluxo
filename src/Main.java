ce50ea582fc36495573b469759f781cff64b3717
Emerson <Emersonwpprado@gmail.com>
2024-09-30 17:23:12 -0300

    Tarefa Ebac

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,17 @@ public class Main {
     public static void main(String[] args) {
 
         int media1 = 7;
-        int media2 = 8;
+        int media2 = 10;
         int media3 = 5;
-        int media4 = 10;
-        double mediaFinal = (double)(media1 + media2 + media3 + media4) / 4;
+        int media4 = 8;
+        double mediaFinal = (double) (media1 + media2 + media3 + media4) / 4;
 
-        System.out.println(mediaFinal);
+        if(mediaFinal > 7) {
+            System.out.printf("O aluno foi aprovado com a nota %.2f", mediaFinal);
+        } else if (mediaFinal >=5 && mediaFinal < 7) {
+            System.out.printf("O aluno está de recuperação com a nota %.2f", mediaFinal);
+        } else if (mediaFinal <5) {
+            System.out.printf("O aluno foi reprovado com a nota %.2f", mediaFinal);
+        }
     }
 }
